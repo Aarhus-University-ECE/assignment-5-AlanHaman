3eadf64eb466f3f8f8d98923f473ffc3cb953418
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-02 21:51:12 +0200

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,18 +1,35 @@
 #include <malloc.h>
 #include "circle.h"
-
+#include <stdbool.h>
 
 void fiveCircles(circle c[]) {
 /*post: returns an array with five circles - solution to 6.b*/
+for (int i=0; i<5; i++){
+    struct circle c[4];
+    c[i].r=i;
+    c[i].p.x=i;
+    c[i].p.y=i;
+    printf("\nradius: %d, x:%d, y:%d, ",c[i].r, c[i].p.x, c[i].p.y);
+  }
 }
 
-int circleIsValid(const circle * c) {
+bool circleIsValid(const circle * c) {
 /*post: answer to exercise 6.c*/
-	return 0
+  if (c->r>0){
+    return true; 
+    }
+  else{ 
+    return false;
+  }
+  return 0;
 }
+	
 
 void translate(circle* c, const point* p) {
-/*ansver to exercise 6.d
+/*ansver to exercise 6.d*/
+
+c[0].p.x=p->x+c[0].p.x;
+c[0].p.y=p->y+c[0].p.y;
 }
 
 
