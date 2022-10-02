6e3bb6d9002de940ce630df4ab1d087981f1125d
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-02 21:51:44 +0200

    Update circle.h

--- circle.h
+++ circle.h
@@ -4,13 +4,13 @@ typedef struct point {
 	int y;
 } point;
 
+/*add your definition of circle here. You need top call it "circle"*/
+
 typedef struct circle {
   int r;
 point p;
 
 }circle;
-/*add your definition of circle here. You need top call it "circle"*/
-
 void fiveCircles(circle c[]);
 int circleIsValid(const circle* c);
 void translate(circle* c, const point* p);
