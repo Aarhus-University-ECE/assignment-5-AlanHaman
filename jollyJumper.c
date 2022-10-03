f42b58a7e2639b23afbc7dbb2b9d4e759670cb8d
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-03 19:19:39 +0200

    Update jollyJumper.c

--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,36 @@
 #include "jollyjumper.h"
 #include <stdbool.h>
 #include <malloc.h>
+#include <stdlib.h>
+#include <assert.h>
 
-,mbj
 
 int isJollyJumber(const int seq[], int size) {
+    bool diffs_found[99];
 /*post: answer to exercise 7.a*/
-    return 1;
+for (int i =1;i<size;i++){
+int current = seq[i];
+int previous = seq [i-1];
+int  dif =  current - previous;
+if (dif <0){
+    dif = dif* -1;
 }
 
+diffs_found [dif]=true;
+
+
+if (dif ==0||dif >=size){
+printf("Not a jolly jumper \n");
+}
+else 
+{   
+  printf ("is a jollyjumper\n");
+  }
+return 0; 
+
+
+
+}
+
+    return 1;
+}
