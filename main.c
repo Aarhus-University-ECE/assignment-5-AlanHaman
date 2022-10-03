f97ef936fd6ac913e3c8fab46ec9b94ef3a13d23
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-03 19:17:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<malloc.h>
 #include "circle.h"
 #include"jollyjumper.h"
+#include <assert.h>
 
 void printCircle(circle c) {
 	printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
@@ -9,7 +10,7 @@ void printCircle(circle c) {
 
 int main(void) {
 	/*testing circles - make sure that you know how it works and what it is supposed to print out*/
-	circle c[5];
+	 circle c[5];
 	fiveCircles(c);
 	for (int i = 0; i < 5; i++) {
 		printCircle(c[i]);
@@ -21,18 +22,16 @@ int main(void) {
 	printCircle(c[1]);
 	printf("isValid: %d", circleIsValid(&c[1]));
 
-	/*answer to exercise 7.b*/
-	int n; /*number of numbers to read*/
-	/*readin n and check that is is OK*/
-
-	int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
 
 	/*readin the n numbers in the array numbers*/
-
-	if (isJollyJumber(numbers, n)) {
-		printf("it is a Jolly Jumper");}
-	else {
-		printf("not a Jolly Jumper");}
+// I decided to make some testcases instead of input
+	int seq1[] = {1,4,2,3};
+	isJollyJumber(seq1,4);
+    int seq2 []= {3,4,2,5,1};
+	isJollyJumber(seq2,5);
+	int seq3 []= {7,4,2,5,1};
+	isJollyJumber(seq3,5);
+	
 	return 0;
 
 }
