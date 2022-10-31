c4311770b61d2ab3570e821f5d79df76f21854e5
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-31 21:01:29 +0100

    Update jollyJumper.c

--- jollyJumper.c
+++ jollyJumper.c
@@ -6,24 +6,39 @@
 
 
 int isJollyJumber(const int seq[], int size) {
-  int array[size - 1];
-  for (int i = 0; i < size - 1; i++)
-    array[i] = 0;
-    
-// loop to calculate the difference between 2 adjacent numbers 
-  for (int i = 0; i < size - 1; i++) {
-    int sum = (seq[i] > seq[i + 1]);
-    seq[i] - seq[i + 1];
-    seq[i + 1] - seq[i];
-      
-// to check if we have a jullyjumper
-    if (sum > 0 && sum < size)
-      array[sum - 1] = 1;
-  }
+/*post: answer to exercise 7.a*/
 
-  for (int i = 0; i < size; i++) {
-    if (array[i] == 0)
-      return 0;
-  }
-  return 1;
+int diff; 
+bool diffs_found[size-1]; 
+
+for (int i = 0; i < size; i++) // for loop
+{
+    diffs_found[i] = false;
+}
+
+
+for (int i = 0; i<size-1; i++){
+    diff = seq[i] - seq[i+1]; // Calculates the difference of the first and next number in the sequence
+    if (diff < 0) // If-else statement, checks if difference is negative, 
+    {
+        diff = -diff;
+    }
+
+    if (diff == 0 || diff >= size || diffs_found[diff-1] == true){ //if- else statement
+        break;
+    }
+    else{
+        diffs_found[diff-1] = true; // Sets value checked to be true if preconitions are fulfilled
+    }
+}
+
+for (int i = 0; i < size-1; i++) 
+{
+    if (diffs_found[i] == false)
+    {
+        return 0;
+    }
+}
+
+return 1;
 }
